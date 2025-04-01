a37b8648e574684fa2add64256388fc23cc29578
sai-ram-kaushik <sairamkaushik1@gmail.com>
2025-04-01 18:32:21 +0530

    updated with the required changeds

--- constants/index.js
+++ constants/index.js
@@ -28,25 +28,12 @@ export const menuList = [
 
 export const about_list = [
   {
-    label: "Comprehensive Financial Services",
-    key: "Comprehensive Financial Services",
-    icon: <MdBusinessCenter size={70} />,
-    desc: "GOOLLUCK INVESTMENTS serves as your reliable and comprehensive financial service provider. We understand the importance of your financial goals and strive to offer tailored solutions. With years of industry experience, we are committed to assisting individuals and businesses in achieving financial success.",
-  },
-
-  {
     label: "Leading Financial Training Institute",
     key: "Leading Financial Training Institute",
     icon: <BiSolidSchool size={70} />,
-    desc: "As a leading financial training institute, we are dedicated to empowering individuals and professionals. Our focus is on providing knowledge and skills essential for excelling in the dynamic world of finance. Through a commitment to excellence, innovation, and a passion for education, we have become a trusted resource for financial education and training.",
+    desc: "As a leading financial training institute, we are dedicated to empowering individuals and professionals. Our focus is on providing knowledge and skills essential for excelling in the dynamic world of finance. Through a commitment to excellence, innovation, and a passion for education, we have become a trusted resource for education and training.",
   },
 
-  {
-    label: "Commitment to Excellence and Innovation",
-    key: "Commitment to Excellence and Innovation",
-    icon: <BsFillPersonFill size={60} />,
-    desc: "At GOOLLUCK INVESTMENTS, our commitment to excellence and innovation sets us apart. We are not just financial experts but also dedicated educators, ensuring you have the tools for success. Count on us as a trusted partner in your financial journey, where a passion for education drives our commitment to your financial well-being.",
-  },
 ];
 
 export const socials = [
--- sections/HomePage.jsx
+++ sections/HomePage.jsx
@@ -1,24 +1,24 @@
+/** @format */
+
 import Image from "next/image";
 import vector from "@/public/assets/8.png";
 import Button from "@/components/Button";
 import TransitionEffect from "@/components/TransitionEffect";
 const HomePage = () => {
   return (
-    <section className="w-full py-10 p-5">
+    <section className='w-full py-10 p-5'>
       <TransitionEffect />
       {/* <Image src={design} alt='design' className='absolute top-0 left-0' /> */}
-      <div className="container mx-auto w-full h-full">
-        <div className="flex flex-col lg:flex-row items-center justify-between gap-20 lg:gap-40 w-full h-full">
-          <div className="flex flex-col items-center lg:items-start lg:w-[649.81px] gap-[50px]">
-            <h1 className="leading-[2.5rem] lg:leading-[4.5rem]">
+      <div className='container mx-auto w-full h-full'>
+        <div className='flex flex-col lg:flex-row items-center justify-between gap-20 lg:gap-40 w-full h-full'>
+          <div className='flex flex-col items-center lg:items-start lg:w-[649.81px] gap-[50px]'>
+            <h1 className='leading-[2.5rem] lg:leading-[4.5rem]'>
               Goolluck Investments -{" "}
               <span>Stock Market Training Institute</span>
             </h1>
 
-            <p className="text-justify lg:text-start">
-              Goolluck Investments is a portfolio advisory firm and stock market
-              training institute. Here, you will learn about real time trading
-              and strategies used by the traders of our institution. Learn
+            <p className='text-justify lg:text-start'>
+              Goolluck Investments is a stock market training institute. Learn
               directly from trader. Come & learn directly from traders.
             </p>
 
@@ -28,10 +28,10 @@ const HomePage = () => {
           <div>
             <Image
               src={vector}
-              alt="home_vector"
+              alt='home_vector'
               width={700}
               height={700}
-              className="rounded-xl"
+              className='rounded-xl'
             />
           </div>
         </div>
