7eae402f24408cd3f0caa56358583f1bd65fad9b
sai-ram-kaushik <sairamkaushik1@gmail.com>
2025-04-05 19:41:33 +0530

    updated with the home text

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
+/** @format */
+
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      {/* <Component {...pageProps} /> */}
+      <div className='flex items-center justify-center w-full h-screen lg:h-[80vh]'>
+        <h1>This Website is under maintainance</h1>
+      </div>
     </Layout>
   );
 }
