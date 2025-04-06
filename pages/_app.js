31c7c4f60eea14e74285c1773be917277ef1e238
sai-ram-kaushik <sairamkaushik1@gmail.com>
2025-04-06 13:10:15 +0530

    removed layout wrapping from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,11 @@ import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
+    <>
       {/* <Component {...pageProps} /> */}
-      <div className='flex items-center justify-center w-full h-screen lg:h-[80vh]'>
+      <div className='flex items-center justify-center w-full h-screen'>
         <h1>This Website is under maintainance</h1>
       </div>
-    </Layout>
+    </>
   );
 }
